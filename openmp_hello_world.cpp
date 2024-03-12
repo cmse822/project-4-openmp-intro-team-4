89417ae8024c1570e64953c7a2304523c5ea92c4
Chris Gerlach <gerlac37@css-118.i>
2024-03-12 11:22:31 -0400

    Added comment of compiling helloworld in cpp file

--- openmp_hello_world.cpp
+++ openmp_hello_world.cpp
@@ -13,9 +13,26 @@ int main(int argc, char* argv[])
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    cout << "Hello, World! I am process " << rank << " of " << numtasks << endl;
+
+#pragma omp parallel
+{
+    // if (rank == numtasks-1){
+        cout << "Hello, World! I am process " << rank << " of " << numtasks << endl;
+    // }
+}
+
     MPI_Finalize();
 
     cout << "After initialize" << endl;
     return 0;
 }
+
+
+/*
+Compile with
+mpicxx -fopenmp hello.cpp 
+
+determine number of threads by
+export OMP_NUM_THREADS=2
+./a.out
+*/
