eace164aa93cc5189e9d36c4a7b231ba78e5e923
chaaran757 <47863104+chaaran757@users.noreply.github.com>
2024-03-26 03:44:31 -0400

    Added debug code

--- Part_3/matrix_multiplication_Part3.cpp
+++ Part_3/matrix_multiplication_Part3.cpp
@@ -167,6 +167,8 @@ int main(int argc, char *argv[]) {
                     request_counter++;
                 }
 
+            cout << num_rows << " " << request_counter;
+
             // Wait for all non-blocking operations to complete
             MPI_Waitall(request_counter, requests, MPI_STATUS_IGNORE);
         }
