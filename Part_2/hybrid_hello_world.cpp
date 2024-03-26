4223fbca2b1827418d9ebbbf2e10f66d74e85d09
chaaran757 <47863104+chaaran757@users.noreply.github.com>
2024-03-26 02:09:38 -0400

    Added code for part 2

---
+++ Part_2/hybrid_hello_world.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <mpi.h>
+#include <omp.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) 
+{
+    int numtasks, rank;
+
+    cout << "Before initialization" << endl;
+    
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    #pragma omp parallel
+    {
+        const id = omp_get_thread_num();
+        const num_threads = omp_get_num_threads();
+        cout << "This is thread " << id + 1 << " of " << num_threads << " in rank " << rank << endl;
+    }
+    
+    MPI_Finalize();
+    cout << "After initialize" << endl;
+
+    return 0;
+}
